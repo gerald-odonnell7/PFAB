ceb362402e22fe726e2ec9cce034d42c05ae9af2
rwdorsey <rwdorsey111@gmail.com>
2014-06-30 21:00:45 -0500

    Create Homework2A.py

---
+++ Homework2A.py
@@ -0,0 +1,8 @@
+cost = input("What is the cost of the item?")
+
+rate = input("What is the sales tax rate--(Expressed as 0.XX)")
+
+tax = cost * rate
+price = cost + tax
+
+print "The full cost of the item is $", price
